0824e4178ddfa6e546e30225da74275178b57e51
the_lux <77723343+the-lux@users.noreply.github.com>
2024-05-03 20:09:08 +0200

    GenMap code

---
+++ GeneratoreMappaProgetto/src/controller/MainFrameController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.MapGen;
+import view.mainFrame;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MainFrameController {
+    public MainFrameController(mainFrame f, MapGen m){
+        f.addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {}
+            public void keyReleased(KeyEvent e) {}
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_SPACE){
+                    //f.removeAll();
+                    f.getContentPane().removeAll();
+                    f.setN(0);
+                    f.newGen(m.genMap(10));
+                    System.out.println(m.getRoot().toString());
+                }
+                if(e.getKeyCode() == KeyEvent.VK_P){
+
+                }
+            }
+        });
+    }
+}
