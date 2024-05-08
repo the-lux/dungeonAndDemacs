7966082f10bb1bff78763c9d808129de6c0acb1c
the_lux <77723343+the-lux@users.noreply.github.com>
2024-05-08 16:07:07 +0200

    MainFolder

---
+++ DungeonsAndDemacs/src/application/Main.java
@@ -0,0 +1,19 @@
+import controller.MainFrameController;
+import controller.MainFrameController;
+import model.MapGen;
+import view.*;
+
+//import java.awt.event.KeyEvent;
+//import java.awt.event.KeyListener;
+
+public class Main {
+    public static void main(String[] args) {
+        MapGen m = new MapGen();
+        m.genMap(10);
+        mainFrame f = new mainFrame();
+        f.newGen(m.getRoot());
+        MainFrameController fC = new MainFrameController(f,m);
+
+        f.setVisible(true);
+    }
+}
