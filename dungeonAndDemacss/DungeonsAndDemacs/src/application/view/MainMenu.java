5cc09fc86274aedeb98e48c0b68874f1c6000595
superLuca <luca.romano03@tiscali.it>
2024-06-07 18:02:20 +0200

    Aggiunta menu da finire

---
+++ dungeonAndDemacss/DungeonsAndDemacs/src/application/view/MainMenu.java
@@ -0,0 +1,21 @@
+package application.view;
+import application.config.Settings;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenu extends JFrame {
+
+    public MainMenu() {
+        setSize(Settings.WINDOW_SIZE, Settings.WINDOW_SIZE);
+        Dimension dim=Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width/2-getSize().width/2, dim.height/2-getSize().height/2);
+        MenuPanel m = new MenuPanel();
+        add(m);
+        setUndecorated(true);
+        m.setFocusable(true);
+        //m.addKeyListener(new CharacterListener(worldPanel));
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
