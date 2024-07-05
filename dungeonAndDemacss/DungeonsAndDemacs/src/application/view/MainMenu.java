842c9d6a202e8bd8cb4ae0dd93e97ec5ba611027
the_lux <luca.romano03@tiscali.it>
2024-07-05 11:47:04 +0200

    Aggiunto il pannello opzioni comandi (non visualizza il gradient)

--- dungeonAndDemacss/DungeonsAndDemacs/src/application/view/MainMenu.java
+++ dungeonAndDemacss/DungeonsAndDemacs/src/application/view/MainMenu.java
@@ -1,7 +1,5 @@
 package application.view;
-import application.GameLoop;
 import application.config.Settings;
-import application.controller.CharacterListener;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,15 +10,14 @@ public class MainMenu extends JFrame {
         this.setSize(Settings.WINDOW_SIZE, Settings.WINDOW_SIZE);
         Dimension dim=Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width/2-getSize().width/2, dim.height/2-getSize().height/2);
-        WorldPanel m = new WorldPanel();
+        PanelMenager.getInstance().start(this);
+        /*WorldPanel m = new WorldPanel();
         add(m);
-        setUndecorated(true);
         m.setFocusable(true);
         CharacterListener controller=new CharacterListener(m);
-        m.addKeyListener(controller);
+        m.addKeyListener(controller);*/
+        setUndecorated(true);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        GameLoop gameLoop = new GameLoop(controller);
-        gameLoop.startGame();
     }
 }
