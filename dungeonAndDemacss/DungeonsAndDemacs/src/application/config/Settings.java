1928572ebcbf5fbec09b54e99437ebb9aab8cfa4
MarianoVIsabella <marianovisabella@libero.it>
2024-07-10 23:34:15 +0200

    -Ora Setting è un record

--- dungeonAndDemacss/DungeonsAndDemacs/src/application/config/Settings.java
+++ dungeonAndDemacss/DungeonsAndDemacs/src/application/config/Settings.java
@@ -1,6 +1,6 @@
 package application.config;
 
-public class Settings {
+public record Settings() {
     public final static int WINDOW_SIZE = 600;
     public final static int WORLD_SIZE = 40;
     public final static int BLOCK_SIZE = WINDOW_SIZE / WORLD_SIZE; //15
